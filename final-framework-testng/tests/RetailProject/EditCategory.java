3dc56308e3134b7eb6a06f965ec776a139a68728
SaritaKumari <SaritaKumari@9.102.0.51>
2020-07-05 21:44:56 +0530

    Latest Medium level project submission.

--- final-framework-testng/tests/RetailProject/EditCategory.java
+++ final-framework-testng/tests/RetailProject/EditCategory.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
